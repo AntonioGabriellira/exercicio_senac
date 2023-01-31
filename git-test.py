47faddc246ebd96050e51a98a42749e101ecaf06
gabriel <antoniogabrielsousalira2004@gmail.com>
2023-01-31 20:47:54 -0300

    teste

--- git-test.py
+++ git-test.py
@@ -3,7 +3,8 @@ class Produto:
         self.nome = nome
         self.preco = preco
         self.qtd = qtd
-        
+
 nome = input("nome do produto: ")
 preco = int(input("preço do produto: "))
 qtd = int(input("digite a quantidade: "))
+produto = Produto(nome, preco, qtd)
