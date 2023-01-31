584d5e71ce8123801b4077e8ef5b50b053cb23b5
gabriel <antoniogabrielsousalira2004@gmail.com>
2023-01-31 20:47:18 -0300

    add inputs para a classe
    add nome, preço e qtd para a classe

---
+++ git-test.py
@@ -0,0 +1,9 @@
+class Produto:
+    def __init__(self, nome, preco, qtd):
+        self.nome = nome
+        self.preco = preco
+        self.qtd = qtd
+        
+nome = input("nome do produto: ")
+preco = int(input("preço do produto: "))
+qtd = int(input("digite a quantidade: "))
