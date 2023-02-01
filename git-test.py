660cbd66a2b52fb8c80cf1e0ef6e5c9b8cf97e78
gabriel <antoniogabrielsousalira2004@gmail.com>
2023-02-01 19:55:01 -0300

    add: listamento dos produtos

--- git-test.py
+++ git-test.py
@@ -4,14 +4,21 @@ class Produto:
         self.preco = preco
         self.qtd = qtd
 
-    def exibir(self):
-        print(f" produto: {self.nome} -- preço: {self.preco} -- quantidade: {self.qtd}")
+    def exibir(self, indice = 0):
+        print(f" {indice} === produto: {self.nome} -- preço: {self.preco} -- quantidade: {self.qtd}")
 
-nome = input("nome do produto: ")
+produto_um = Produto("massa", 5, 400)
+produto_dois = Produto("tomate", 2, 100)
+produtos = [produto_um, produto_dois]
+for produto in produtos:
+    indice = produtos.index(produto)
+    produto.exibir(indice)
 preco = int(input("preço do produto: "))
 qtd = int(input("digite a quantidade: "))
-produto = Produto(nome, preco, qtd)
-
-produto.exibir()
-
-print(f"o valor total é: {qtd * produto.preco}")
+indice_selecionado = int(input("selecione o produto: "))
+if indice_selecionado > len(produtos) - 1:
+    print("produto não encontrado")
+else:
+    produto = produtos[indice_selecionado]
+    quantidade = int(input("digite a quantidade: "))
+    print(f"o valor total é: {quantidade * produto}")
